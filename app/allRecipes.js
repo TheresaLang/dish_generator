b6dc42391050526dd3581ea4b63fccaea7081641
Theresa Lang <theresa.lang@gmx.net>
2023-12-17 15:48:17 +0100

    Extract function returning list of recipes

---
+++ app/allRecipes.js
@@ -0,0 +1,16 @@
+function getAllRecipes() {
+    return [
+        "Risotto",
+        "Lasagne",
+        "Ofengemüse",
+        "Penne Arrabiata",
+        "Belugalinsensalat",
+        "Tofu-Geschnetzeltes",
+        "Petersilien-Zitronen-Spaghetti",
+        "Linseneintopf",
+        "Semmelknödel mit Pilz-Rahm-Soße",
+        "Sommerrollen"
+    ]
+}
+
+export default getAllRecipes
